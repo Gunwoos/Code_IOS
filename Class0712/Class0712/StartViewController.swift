79b8a0259e6de2e1add418160637965fa1ba44d5
Gun woo Lim <lkwyoyo@gmail.com>
2018-07-12 17:52:40 +0900

    edit

---
+++ Class0712/Class0712/StartViewController.swift
@@ -0,0 +1,28 @@
+//
+//  StartViewController.swift
+//  Class0712
+//
+//  Created by 임건우 on 2018. 7. 12..
+//  Copyright © 2018년 lims. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
--- Class0712/Class0712/ViewController.swift
+++ Class0712/Class0712/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dontKnowUrlButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var placeholderLabel: UILabel!
+    @IBOutlet weak var placeholderLabelLeadingCOnstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        underlineStyleByProgrammticallyExample()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    
+    // 궁금한 부분 + programmatically 로 검색하면 코드 나옴
+    private func underlineStyleByProgrammticallyExample(){
+        // Plain
+//        let buttonTitle = dontKnowUrlButton.title(fro: .normal)!
+//        let mutableAttrStr = NSMutableAttributedString(string: buttonTitle)
+        
+        // Attributed
+        guard let buttonTitle = dontKnowUrlButton.currentAttributedTitle else {return}
+        let mutableAttrStr = NSMutableAttributedString(attributedString: buttonTitle)
+        let attrKeys: [NSAttributedStringKey: Any] = [
+            .foregroundColor: UIColor.darkText,
+            .underlineStyle: NSUnderlineStyle.styleSingle.rawValue,
+            .underlineColor: UIColor.darkGray
+        ]
+        mutableAttrStr.addAttributes(attrKeys, range: NSRange(location: 0, length: buttonTitle.string.count))
+        dontKnowUrlButton.setAttributedTitle(mutableAttrStr, for: .normal)
+    }
 
 }
 
+
+// MARK: - UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let text = textField.text ?? ""
+        let replacedText = (text as NSString).replacingCharacters(in: range, with: string)
+        guard replacedText.count < 35 else { return false }
+        
+        let textFieldAttr = [NSAttributedStringKey.font: textField.font!]
+        let textWidthSize = (replacedText as NSString).size(withAttributes: textFieldAttr).width
+        placeholderLabelLeadingCOnstraint.constant = textWidthSize
+        placeholderLabel.text = replacedText.isEmpty ? "workspace-url.slack.com" : ".slack.com"
+        
+        return true
+    }
+}
